b14ac3374b4f28be6119fb6c8ca14b4ace0ce3d6
IanJCV <ausp.jcv@gmail.com>
2023-01-17 08:24:30 +0100

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,14 +1,27 @@
 var latestTurn = -1;
 
 Hooks.on('combatTurn', (combat, data, options) => {
-    if (latestTurn >= data.turn || options.direction < 0) return;
+    if (latestTurn >= data.turn || options.direction < 0 || data.round <= 1) return;
 
-    let actor = Actor.get(game.combat.combatants._source[data.turn].actorId);
-    let props = actor.system.props;
-    
     const update = {};
-    update['system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(props.spregen), parseInt(props.spmax));
-    actor.update(update);
+    let combatant = game.combat.nextCombatant;
+    let token = canvas.tokens.get(combatant.tokenId);
+    let document = token.document;
+    if (token.document.actorLink)
+    {
+        let actor = Actor.get(game.combat.nextCombatant.actorId);
+        let props = actor.system.props;
+        
+        update['system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(props.spregen), parseInt(props.spmax));
+        actor.update(update);
+    }
+    else
+    {
+        // let props = document.actorData.system.props;
+        // update['actorData.system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(document._actor.system.props.spregen), parseInt(document._actor.system.props.spmax));
+        // document.update(update);
+    }
+
     latestTurn = data.turn;
 });
 
@@ -16,10 +29,22 @@ Hooks.on('combatRound', (combat, data, options) => {
     if (options.direction < 0) return;
     latestTurn = 0;
 
-    let actor = Actor.get(game.combat.combatants._source[data.turn].actorId);
-    let props = actor.system.props;
-    
     const update = {};
-    update['system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(props.spregen), parseInt(props.spmax));
-    actor.update(update);
+    let combatant = game.combat.nextCombatant;
+    let token = canvas.tokens.get(combatant.tokenId);
+    let document = token.document;
+    if (token.document.actorLink)
+    {
+        let actor = Actor.get(game.combat.nextCombatant.actorId);
+        let props = actor.system.props;
+        
+        update['system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(props.spregen), parseInt(props.spmax));
+        actor.update(update);
+    }
+    else
+    {
+        // let props = document.actorData.system.props;
+        // update['actorData.system.props.spcurrent'] = Math.min(parseInt(props.spcurrent) + parseInt(document._actor.system.props.spregen), parseInt(document._actor.system.props.spmax));
+        // document.update(update);
+    }
 });
